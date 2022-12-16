484626d3270e8dc1c30f5dd95a22d292066dbb26
Computer Science Society <111565487+ComputerScienceSoceityNITS@users.noreply.github.com>
2022-12-16 19:29:04 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
-const connectDataBase = () => {
-    mongoose.connect(process.env.DB_URI, {
+const connectDataBase = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI, {
         useNewUrlParser: true, useUnifiedTopology: true,
         // useCreateIndex: true
-    }).then((data) => {
-        console.log(`mongodb connected with server ${data.connection.host}`);
-    })
-    .catch((err) => {   
-        console.log(err.message);
-    })
-
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
 
 module.exports=connectDataBase;
