81d0ca8ee013fd59e9c25b95ef2a8b77dc71a519
Aditya Verma <123adiverma@gmail.com>
2022-12-18 11:51:49 +0530

    cors error

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ if(process.env.NODE_ENV!=="PRODUCTION"){
     require("dotenv").config({ path: "./config/.env" });
 }
 
+// Enable CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+})
+
+
 app.use(cookieparser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
