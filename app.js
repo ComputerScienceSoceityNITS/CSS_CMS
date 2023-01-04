9b6fd7c0a9ad61ad1de1359840d9d99d29214409
Nrittik Sarmah <nrittik43@gmail.com>
2023-01-04 20:06:12 +0530

    trial 3

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ const ALLOWED_ORIGINS = [
 
 app.all("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", process.env.ADMIN_URL);
+  res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
     "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
