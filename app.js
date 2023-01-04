a4574920ee4a0830cb8945c750b6e0c917e52155
Nrittik Sarmah <nrittik43@gmail.com>
2023-01-04 20:02:44 +0530

    trial 1

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const ALLOWED_ORIGINS = [
 // app.use(cors(corsOptions));
 
 app.all("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", ALLOWED_ORIGINS);
   res.header(
     "Access-Control-Allow-Headers",
     "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
