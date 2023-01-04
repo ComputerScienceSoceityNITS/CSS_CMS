1e8098e9f4e90282d5ae9173ac32ad834fd417e7
Aditya Verma <78556885+vermastra@users.noreply.github.com>
2023-01-04 11:54:04 +0530

    cors update

--- app.js
+++ app.js
@@ -11,10 +11,9 @@ if(process.env.NODE_ENV!=="PRODUCTION"){
 
 // Enable CORS
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.ORIGIN_URL);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", true);
     next();
 })
 
