83b46a9c9dfecfc7fa601cc1b6145b0c16c2aee4
Nrittik Sarmah <nrittik43@gmail.com>
2023-01-04 20:45:51 +0530

    trial 6

--- app.js
+++ app.js
@@ -10,28 +10,23 @@ if(process.env.NODE_ENV!=="PRODUCTION"){
     require("dotenv").config({ path: "./config/.env" });
 }
 
-const ALLOWED_ORIGINS = [
+//cors
+const allowlist = [
   "http://localhost:3000",
   process.env.CLIENT_URL,
   process.env.ADMIN_URL,
 ];
-
-// enable CORS
-const corsOptions = {
-  origin: ALLOWED_ORIGINS,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
-  allowedHeaders: [
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "Authorization",
-  ],
-  credentials: true,
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
 };
 
-app.options(ALLOWED_ORIGINS, cors(corsOptions));
-app.use(cors(corsOptions));
+app.use(cors(corsOptionsDelegate));
 
 app.use(cookieparser())
 app.use(express.json())
