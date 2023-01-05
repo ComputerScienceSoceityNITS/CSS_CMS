8741c401223b895e069af5ba94a305a3133014e8
Nrittik Sarmah <nrittik43@gmail.com>
2023-01-05 09:58:44 +0530

    trial 11

--- app.js
+++ app.js
@@ -10,47 +10,28 @@ if(process.env.NODE_ENV!=="PRODUCTION"){
     require("dotenv").config({ path: "./config/.env" });
 }
 
-//cors
-// const allowlist = [
-//   "http://localhost:3000",
-//   process.env.CLIENT_URL,
-//   process.env.ADMIN_URL,
-// ];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (allowlist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
-//   allowedHeaders: [
-//     "Origin",
-//     "X-Requested-With",
-//     "Content-Type",
-//     "Accept",
-//     "Authorization",
-//   ],
-//   credentials: true,
-// };
-
-// app.use(cors(corsOptions));
-
-// Enable CORS
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.ADMIN_URL);
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
-})
-
 app.use(cookieparser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(fileUpload({useTempFiles:true}))
+app.use(fileUpload({ useTempFiles: true }))
+
+// enable cors
+const ALLOWED_ORIGINS = ["http://localhost:3000", process.env.CLIENT_URL, process.env.ADMIN_URL];
+
+app.use(cors({ origin: ALLOWED_ORIGINS, credentials: true }));
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const theOrigin = ALLOWED_ORIGINS.indexOf(origin) >= 0 ? origin : ALLOWED_ORIGINS[0];
+  res.header("Access-Control-Allow-Origin", theOrigin);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+  next();
+});
+
+app.options("*", cors());
 
 const members = require("./routes/membersRoute.js");
 const events = require("./routes/eventsRoute.js");
